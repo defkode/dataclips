b86fadb84637f8069c862348e98e9936f54d1901
Tomasz Mazur <defkode@gmail.com>
2015-03-17 14:46:15 +0100

    wip

--- app/models/dataclips/clip.rb
+++ app/models/dataclips/clip.rb
@@ -24,15 +24,15 @@ module Dataclips
       attributes.reduce({}) do |memo, (key, value)|
         if schema_key = self.class.schema[key]
           memo[key] = case schema_key[:type].to_sym
-            when :text, :string        then ActiveRecord::Type::String.new.type_cast_from_database(value)
-            when :integer              then ActiveRecord::Type::Integer.new.type_cast_from_database(value)
-            when :float                then ActiveRecord::Type::Float.new.type_cast_from_database(value)
-            when :decimal              then ActiveRecord::Type::Decimal.new.type_cast_from_database(value)
-            when :datetime             then ActiveRecord::Type::DateTime.new.type_cast_from_database(value)
-            when :time                 then ActiveRecord::Type::Time.new.type_cast_from_database(value)
-            when :date                 then ActiveRecord::Type::Date.new.type_cast_from_database(value)
-            when :binary               then ActiveRecord::Type::Binary.new.type_cast_from_database(value)
-            when :boolean              then ActiveRecord::Type::Boolean.new.type_cast_from_database(value)
+            when :text     then ActiveRecord::Type::String.new.type_cast_from_database(value)
+            when :integer  then ActiveRecord::Type::Integer.new.type_cast_from_database(value)
+            when :float    then ActiveRecord::Type::Float.new.type_cast_from_database(value)
+            when :decimal  then ActiveRecord::Type::Decimal.new.type_cast_from_database(value)
+            when :datetime then ActiveRecord::Type::DateTime.new.type_cast_from_database(value)
+            when :time     then ActiveRecord::Type::Time.new.type_cast_from_database(value)
+            when :date     then ActiveRecord::Type::Date.new.type_cast_from_database(value)
+            when :binary   then ActiveRecord::Type::Binary.new.type_cast_from_database(value)
+            when :boolean  then ActiveRecord::Type::Boolean.new.type_cast_from_database(value)
             else value
             end
         end
