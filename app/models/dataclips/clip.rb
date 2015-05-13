6fa2fb84847685c1d57f6a8074741c0dfd54d5a8
Tomasz Mazur <defkode@gmail.com>
2015-05-13 14:01:25 +0200

    respect order of attributes matching schema

--- app/models/dataclips/clip.rb
+++ app/models/dataclips/clip.rb
@@ -21,22 +21,20 @@ module Dataclips
     end
 
     def type_cast(attributes)
-      attributes.reduce({}) do |memo, (key, value)|
-        if schema_key = self.class.schema[key]
-          memo[key] = case schema_key[:type].to_sym
-            when :text     then ActiveRecord::Type::String.new.type_cast_from_database(value)
-            when :integer  then ActiveRecord::Type::Integer.new.type_cast_from_database(value)
-            when :float    then ActiveRecord::Type::Float.new.type_cast_from_database(value)
-            when :datetime then ActiveRecord::Type::DateTime.new.type_cast_from_database(value)
-            when :time     then ActiveRecord::Type::Time.new.type_cast_from_database(value)
-            when :date     then ActiveRecord::Type::Date.new.type_cast_from_database(value)
-            when :boolean  then ActiveRecord::Type::Boolean.new.type_cast_from_database(value)
-            else value
-            end
-        end
+      self.class.schema.reduce({}) do |memo, (key, schema_key)|
+        value = attributes[key]
+        memo[key] = case schema_key[:type].to_sym
+          when :text     then ActiveRecord::Type::String.new.type_cast_from_database(value)
+          when :integer  then ActiveRecord::Type::Integer.new.type_cast_from_database(value)
+          when :float    then ActiveRecord::Type::Float.new.type_cast_from_database(value)
+          when :datetime then ActiveRecord::Type::DateTime.new.type_cast_from_database(value)
+          when :time     then ActiveRecord::Type::Time.new.type_cast_from_database(value)
+          when :date     then ActiveRecord::Type::Date.new.type_cast_from_database(value)
+          when :boolean  then ActiveRecord::Type::Boolean.new.type_cast_from_database(value)
+          else value
+          end
         memo
       end
-
     end
 
     def id
