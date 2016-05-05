c70ced2adc73aa0c5ef5843101c8c3901608fcbf
Tomasz Mazur <defkode@gmail.com>
2016-05-05 14:20:06 +0200

    remove dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,27 @@
-var gulp, sourcemaps, browserify, coffeeify, source;
+var gulp, sourcemaps, browserify, coffeeify, sass, less, concat, source;
 
-gulp = require('gulp');
+gulp       = require('gulp');
 sourcemaps = require('gulp-sourcemaps');
 browserify = require('browserify');
-coffeeify = require('coffeeify');
-source = require('vinyl-source-stream');
+coffeeify  = require('coffeeify');
+sass       = require("gulp-sass");
+less       = require("gulp-less");
+source     = require('vinyl-source-stream');
+concat     = require('gulp-concat');
 
-gulp.task('default', ['compile-coffee']);
+// gulp.task('default', ['compile-coffee']);
+
+gulp.task('bundle-css', function() {
+  gulp.src(['./node_modules/font-awesome/css/font-awesome.css', './node_modules/bootstrap/dist/css/bootstrap.css', './node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css', './app/assets/stylesheets/dataclips/dataclips.css'])
+      .pipe(concat('application.css'))
+      .pipe(gulp.dest('./app/assets/stylesheets/dataclips'));
+});
+
+gulp.task('compile-sass', function() {
+    gulp.src('./app/assets/stylesheets/dataclips/dataclips.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/assets/stylesheets/dataclips'));
+});
 
 gulp.task('compile-coffee', function() {
   var stream = browserify('./app/assets/javascripts/dataclips/src/main.js',
@@ -17,5 +32,5 @@ gulp.task('compile-coffee', function() {
    .bundle();
 
   stream.pipe(source('bundle.js'))
-        .pipe(gulp.dest('./app/assets/javascripts/dataclips/dist'));
+    .pipe(gulp.dest('./app/assets/javascripts/dataclips/dist'));
 });
