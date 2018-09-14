84dae47bc63fbb78e2e79f13319bf8bf68c4ea76
Kuba Krzempek <kubakrzempek@gmail.com>
2018-09-14 10:30:14 +0200

    Add configuration block with default params to Insights (#52)
    * Change dummy app config to postgres
    
    Without the config (and [4.2] included in the migration file) it is
    impossible to bootstrap the dummy app, which in turn is required to run
    tests. This commit fixes that.
    
    * Add configuration block with default params to Insights
    
    With the config block it becomes possible to attach default params, so
    dataclips can be scoped and run in a certain context, for example in SaaS
    environments. It simplifies usage in a great deal because one doesn't
    have to pass the same param over and over again. It also supports dynamic
    values evaluated on-the-fly.
    
    I've used separate configure block (instead of `Dataclips::Engine`) to
    loosen a little the hard dependency on Rails. The gem might be used in
    other envs as well, so it doesn't have to be tied that hard to Rails.

--- app/models/dataclips/insight.rb
+++ app/models/dataclips/insight.rb
@@ -1,6 +1,25 @@
 module Dataclips
   class Insight < ActiveRecord::Base
     class FileNotFound < ArgumentError; end
+    class Config
+      # @return [Hash{Symbol=>Any}] default parameters that can be set app-wise (and changed if required). Supports dynamic values (via callable objects)
+      # @api public
+      attr_accessor :default_params
+
+      def initialize(user_config = {})
+        self.default_params = user_config[:default_params] || {}
+      end
+    end
+
+    class << self
+      def configure
+        yield @config = Config.new
+      end
+
+      def config
+        @config ||= Config.new
+      end
+    end
 
     validates :clip_id, presence: true
     validates :hash_id, presence: true, uniqueness: true
@@ -21,9 +40,14 @@ module Dataclips
     end
 
     def self.get!(clip_id, params = {}, options = {})
+      params = {
+        **config.default_params,
+        **params.symbolize_keys,
+      }.transform_values { |v| v.respond_to?(:call) ? v.call : v }
+
       schema    = options[:schema]
 
-      clip      = Clip.new(clip_id, schema) 
+      clip      = Clip.new(clip_id, schema)
       name      = options.fetch(:name, clip.name || clip_id)
 
       checksum = calculate_checksum(clip_id, params, schema)
@@ -31,7 +55,7 @@ module Dataclips
         return insight
       else
         hash_id = SecureRandom.urlsafe_base64(6)
-        
+
         if basic_auth = options[:basic_auth]
           if basic_auth[:username].present? && basic_auth[:password].present?
             basic_auth_credentials = [basic_auth[:username], basic_auth[:password]].join(":")
