b741cc198fa82b3b8c29ce34597e941aad469358
Tomasz Mazur <defkode@gmail.com>
2018-12-10 13:58:44 +0100

    wip

--- db/migrate/20150101143530_create_dataclips_insights.rb
+++ db/migrate/20150101143530_create_dataclips_insights.rb
@@ -1,15 +1,17 @@
 class CreateDataclipsInsights < ActiveRecord::Migration[5.2]
   def change
     create_table :dataclips_insights do |t|
-      t.string  :clip_id, null: false
-      t.string  :schema
       t.string  :hash_id, null: false
+
+      t.string  :clip_id, null: false
+      t.json    :params
+
       t.string  :checksum, null: false
       t.string  :time_zone, null: false
       t.string  :name
+      t.string  :connection
+      t.string  :schema
       t.string  :basic_auth_credentials
-
-      t.json    :params
       t.datetime :last_viewed_at
       t.timestamps
     end
--- lib/dataclips/engine.rb
+++ lib/dataclips/engine.rb
@@ -3,14 +3,7 @@ module Dataclips
     isolate_namespace Dataclips
 
     config.before_configuration do
-      config.dictionaries = {}
-      config.path         = Rails.root.join('app/dataclips').to_s
-      config.themes       = {
-        "default" => {
-          "css" => ["//fonts.googleapis.com/css?family=Inconsolata:400,700&subset=latin,latin-ext", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", "dataclips/application.css", "dataclips/dataclips.css"],
-          "progressbar_color" => "#6F5498"
-        }
-      }
+      config.path = Rails.root.join('app/dataclips').to_s
     end
   end
 end
