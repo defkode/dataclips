e13464b0af1959540398c8040f91286e7cdaa6ad
Tomasz Mazur <defkode@gmail.com>
2019-01-03 17:22:44 +0100

    correctly parse ISO8601 without TZ info

--- app/assets/javascripts/dataclips/src/dataclips.js
+++ app/assets/javascripts/dataclips/src/dataclips.js
@@ -35,13 +35,41 @@ export default class Dataclips {
   }
 
 
-  fetchDataInBatches(page = 1, url, reactable) {
+  fetchDataInBatches(page = 1, url, schema) {
+    const ISO8601 = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i
+
     return fetch(url + '?page=' + page).then(function(response) {
       return response.json()
     }).then(function(data) {
-
       const records = data.map(function(i){
-        return JSON.parse(i.record)
+        const parsedRecord = JSON.parse(i.record)
+        let record = {}
+
+        Object.entries(schema).forEach(([schemaKey, options]) => {
+          const recordValue = parsedRecord[schemaKey]
+          if (recordValue !== undefined) {
+            if (options.type === 'datetime') {
+              if (ISO8601.test(recordValue)) {
+                const matches = recordValue.match(ISO8601)
+                const tz      = matches[2]
+                if (tz) {
+                  record[schemaKey] = recordValue
+                } else {
+                  console.warn(`Dataclips: attribute '${schemaKey}' has no TZ information, assuming UTC`)
+                  record[schemaKey] = `${recordValue}Z` // UTC
+                }
+              } else {
+                throw new TypeError(`Dataclips: attribute '${schemaKey}' is valid ISO8601.`)
+              }
+            } else {
+              record[schemaKey] = recordValue
+            }
+
+          } else {
+            throw new TypeError(`Dataclips: attribute '${schemaKey}' is undefined. Please check verify schema.`)
+          }
+        })
+        return record
       })
 
       return {
@@ -57,76 +85,76 @@ export default class Dataclips {
     const { name, schema } = this
     const suggestedFilename = `${name}.xlsx`
 
-    const workbook = Builder.createWorkbook()
+    const filename = prompt('filename', suggestedFilename)
+    if (filename !== null) {
+      const workbook = Builder.createWorkbook()
 
-    const xlsx_number_formats = {
-      datetime_formatter:  {id: 1, numFmtId: 22},
-      duration_formatter:  {id: 2, numFmtId: 46}
-    }
+      const xlsx_number_formats = {
+        datetime_formatter:  {id: 1, numFmtId: 22},
+        duration_formatter:  {id: 2, numFmtId: 46}
+      }
 
-    const stylesheet = workbook.getStyleSheet()
-    stylesheet.fills = [{}, {}] // reset weirdo default styles
+      const stylesheet = workbook.getStyleSheet()
+      stylesheet.fills = [{}, {}] // reset weirdo default styles
 
-    Object.entries(xlsx_number_formats).forEach(([key, format]) => {
-      stylesheet.masterCellFormats.push(format)
-    })
-
-    const sheet = workbook.createWorksheet()
-
-    const headers = Object.keys(schema)
-
-    const rows = []
-
-    rows.push(headers)
-
-    data.forEach((item) => {
-      const row = Object.entries(item).map(([key, value]) => {
-        if (value !== null) {
-          const type = schema[key].type
-
-          switch (type) {
-            case 'boolean':
-              return {
-                value: +value
-              }
-            case 'datetime':
-              const minMs = 60 * 1000
-              const dayMs = (60 * 60 * 24 * 1000)
-
-              const _value = 25569.0 + ((value.ts + (value.offset * minMs)) / dayMs)
-
-              return {
-                value: _value,
-                metadata: {style: xlsx_number_formats.datetime_formatter.id}
-              }
-            case 'duration':
-            case 'time':
-              return {
-                value: value.as('day'),
-                metadata: {style: xlsx_number_formats.duration_formatter.id}
-              }
-
-            default:
-              return value
-          }
-        } else {
-          return null
-        }
+      Object.entries(xlsx_number_formats).forEach(([key, format]) => {
+        stylesheet.masterCellFormats.push(format)
       })
 
-      rows.push(row)
-    })
+      const sheet = workbook.createWorksheet()
 
-    sheet.setData(rows)
-    workbook.addWorksheet(sheet)
+      const headers = Object.keys(schema)
 
-    Builder.createFile(workbook, {type: 'blob'}).then((blobData) => {
-      const filename = prompt('filename', suggestedFilename)
-      if (filename !== null) {
+      const rows = []
+
+      rows.push(headers)
+
+      data.forEach((item) => {
+        const row = Object.entries(item).map(([key, value]) => {
+          if (value !== null) {
+            const type = schema[key].type
+
+            switch (type) {
+              case 'boolean':
+                return {
+                  value: +value
+                }
+              case 'datetime':
+                const minMs = 60 * 1000
+                const dayMs = (60 * 60 * 24 * 1000)
+
+                const _value = 25569.0 + ((value.ts + (value.offset * minMs)) / dayMs)
+
+                return {
+                  value: _value,
+                  metadata: {style: xlsx_number_formats.datetime_formatter.id}
+                }
+              case 'duration':
+              case 'time':
+                return {
+                  value: value.as('day'),
+                  metadata: {style: xlsx_number_formats.duration_formatter.id}
+                }
+
+              default:
+                return value
+            }
+          } else {
+            return null
+          }
+        })
+
+        rows.push(row)
+      })
+
+      sheet.setData(rows)
+      workbook.addWorksheet(sheet)
+
+      Builder.createFile(workbook, {type: 'blob'}).then((blobData) => {
         const blob = new Blob( [blobData], {type: "application/octet-stream"} )
         saveAs(blob, filename)
-      }
-    })
+      })
+    }
   }
 
   init() {
@@ -156,14 +184,14 @@ export default class Dataclips {
         const { data, currentPage, total_count, total_pages} = result
 
         if (currentPage < total_pages) {
-          fetchDataInBatches(currentPage + 1, url, reactable).then(processBatch)
+          fetchDataInBatches(currentPage + 1, url, schema).then(processBatch)
         }
         reactable.addData(result.data, total_count)
       }
 
-      fetchDataInBatches(1, url, reactable).then(processBatch)
+      fetchDataInBatches(1, url, schema).then(processBatch)
     } else {
-      fetchData(url, reactable).then(function(data){
+      fetchData(url).then(function(data){
         reactable.addData(data)
       })
     }
