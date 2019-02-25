165cfd8ac8dcada192148a7c922fdee1bd2ca088
Jakub Cieslar <cieslar.jakub@gmail.com>
2019-02-25 15:24:18 +0100

    dataclips_records

--- lib/dataclips/test/helpers.rb
+++ lib/dataclips/test/helpers.rb
@@ -5,6 +5,11 @@ module Dataclips
         template = File.read("#{Rails.root}/app/dataclips/#{clip_name}.sql")
         PgClip::Query.new(template)
       end
+
+      def dataclips_records(sql)
+        paginator = PgClip::Paginator.new(sql, ActiveRecord::Base.connection)
+        paginator.records
+      end
     end
   end
 end
