303ab545f09d19c6c2358e6b5891ca0c319af496
Tomasz Mazur <defkode@gmail.com>
2019-05-06 14:33:38 +0000

    fix http basic

--- app/controllers/dataclips/insights_controller.rb
+++ app/controllers/dataclips/insights_controller.rb
@@ -3,41 +3,39 @@ require "pg_clip"
 
 module Dataclips
   class InsightsController < ApplicationController
+    before_action :find_and_authenticate_insight
 
-    def show
-      @insight = Dataclips::Insight.find_by_hash_id!(params[:id])
-      authenticate_insight(@insight)
-      @insight.touch(:last_viewed_at)
-    end
+    def show; end
 
     def data
-      @insight = Dataclips::Insight.find_by_hash_id!(params[:id])
-      authenticate_insight(@insight)
-      @insight.touch(:last_viewed_at)
+      respond_to do |format|
+        format.json do
+          @insight.touch(:last_viewed_at)
 
-      template  = File.read("#{Rails.root}/app/dataclips/#{@insight.clip_id}/query.sql")
-      clip      = PgClip::Query.new(template)
-      sql       = clip.query(@insight.params)
+          template  = File.read("#{Rails.root}/app/dataclips/#{@insight.clip_id}/query.sql")
+          clip      = PgClip::Query.new(template)
+          sql       = clip.query(@insight.params)
 
-      paginator = PgClip::Paginator.new(sql, ActiveRecord::Base.connection)
+          paginator = PgClip::Paginator.new(sql, ActiveRecord::Base.connection)
 
-      if per_page = @insight.per_page
-        @result = paginator.execute_paginated_query(sql, page: params['page']&.to_i || 1, per_page: @insight.per_page)
-      else
-        @result = paginator.execute_query(sql)
+          if per_page = @insight.per_page
+            @result = paginator.execute_paginated_query(sql, page: params['page']&.to_i || 1, per_page: @insight.per_page)
+          else
+            @result = paginator.execute_query(sql)
+          end
+
+          render json: @result
+        end
       end
-
-      render json: @result
     end
 
     private
 
-    def authenticate_insight(insight)
-      if insight.basic_auth_credentials.present?
-        if authenticate_with_http_basic { |login, password| insight.authenticate(login, password) }
-        else
-          request_http_basic_authentication
-        end
+    def find_and_authenticate_insight
+      @insight = Dataclips::Insight.find_by!(hash_id: params[:id])
+
+      if @insight.basic_auth_credentials.present?
+        authenticate_or_request_with_http_basic { |login, password| @insight.authenticate(login, password) }
       end
     end
   end
